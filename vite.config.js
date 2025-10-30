c98344d78cb57962aab2b32415b67e375cf8e773
GeoFlow-git <ar.garcia.navarrete@gmail.com>
2025-10-29 21:04:41 -0600

    mi commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'  // esta linea se encarga de importar la función defineConfig desde el paquete 'vite'
+import react from '@vitejs/plugin-react' // esta linea importa el plugin de React para Vite
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base:"/GeoFlow-website",
-
+  base: process.env.NODE_ENV === 'production' ? '/GeoFlow-website/' : '/', // Ajusta esto según el nombre de tu repositorio
 })
 
 
+// Esto define automáticamente el base correcto:
+
+//         /GeoFlow-website/ al hacer npm run build
+//        / al hacer npm run dev
